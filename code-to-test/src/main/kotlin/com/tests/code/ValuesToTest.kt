1b8743f862e93638ef24d943ee9eb70d4e8f06bb
Sergii Gnatiuk <sergii.gnatiuk@form.com>
2023-02-08 00:18:22 +0200

    strikt tests

--- code-to-test/src/main/kotlin/com/tests/code/ValuesToTest.kt
+++ code-to-test/src/main/kotlin/com/tests/code/ValuesToTest.kt
@@ -2,12 +2,40 @@ package com.tests.code
 
 const val STRING_VALUE_TO_TEST = "string value to test"
 
-fun integer() = 0
+val LIST_1_2_3 = listOf(1, 2, 3)
 
-fun list() = listOf("")
+val EMPLOYEE_JOHN = Employee(
+    name = "John",
+    department = Department(
+        name = "R&D",
+        location = Location("Kyiv", listOf("3", "4"))
+    )
+)
 
-fun employee() = Employee()
+val EMPLOYEE_ALICE = Employee(
+    name = "Alice",
+    department = Department(
+        name = "Sales",
+        location = Location("Irpin", listOf("1", "2"))
+    )
+)
 
-class Employee {
+val EMPLOYEES = listOf(EMPLOYEE_JOHN, EMPLOYEE_ALICE)
 
-}
+val MAP_EMPLOYEES = mapOf(1 to EMPLOYEE_JOHN, 2 to EMPLOYEE_ALICE)
+
+data class Location(
+    val value: String,
+    val phones: List<String>
+)
+
+data class Department(
+    val name: String,
+    val location: Location,
+)
+
+data class Employee(
+    val name: String,
+    val department: Department
+)
+
--- strikt/src/test/kotlin/com/tests/strikt/StriktTest.kt
+++ strikt/src/test/kotlin/com/tests/strikt/StriktTest.kt
@@ -1,39 +1,206 @@
 package com.tests.strikt
 
+import com.tests.code.EMPLOYEES
+import com.tests.code.EMPLOYEE_ALICE
+import com.tests.code.EMPLOYEE_JOHN
+import com.tests.code.LIST_1_2_3
+import com.tests.code.MAP_EMPLOYEES
 import com.tests.code.STRING_VALUE_TO_TEST
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import strikt.api.expectThat
+import strikt.assertions.all
+import strikt.assertions.any
+import strikt.assertions.atLeast
+import strikt.assertions.atMost
+import strikt.assertions.containsExactlyInAnyOrder
+import strikt.assertions.containsKey
+import strikt.assertions.containsKeys
+import strikt.assertions.count
+import strikt.assertions.doesNotContainKey
+import strikt.assertions.exactly
+import strikt.assertions.filter
+import strikt.assertions.filterIsInstance
+import strikt.assertions.first
+import strikt.assertions.flatMap
+import strikt.assertions.get
+import strikt.assertions.getValue
+import strikt.assertions.hasEntry
 import strikt.assertions.hasLength
+import strikt.assertions.hasSize
 import strikt.assertions.isBlank
 import strikt.assertions.isEmpty
 import strikt.assertions.isEqualTo
+import strikt.assertions.isEqualToIgnoringCase
+import strikt.assertions.isGreaterThan
+import strikt.assertions.isGreaterThanOrEqualTo
+import strikt.assertions.isLessThan
+import strikt.assertions.isLessThanOrEqualTo
+import strikt.assertions.isNotEmpty
 import strikt.assertions.isNotEqualTo
+import strikt.assertions.isNull
+import strikt.assertions.isNullOrBlank
+import strikt.assertions.isNullOrEmpty
+import strikt.assertions.isSorted
+import strikt.assertions.isUpperCase
+import strikt.assertions.last
+import strikt.assertions.map
 import strikt.assertions.matches
+import strikt.assertions.none
+import strikt.assertions.single
 import strikt.assertions.startsWith
+import strikt.assertions.withElementAt
+import strikt.assertions.withFirst
+import strikt.assertions.withValue
 
 class StriktTest {
 
     @Nested
+    inner class GeneralConceptsTest {
+
+        @Test
+        fun `infix assertions`() {
+            expectThat(1) isNotEqualTo 1
+        }
+
+        @Test
+        fun `assertions with chain`() {
+            expectThat(1)
+                .isNotEqualTo(2)
+                .isGreaterThanOrEqualTo(8)
+        }
+
+        @Test
+        fun `assertions with context(AKA soft assertion)`() {
+            expectThat(1) {
+                isNotEqualTo(0)
+                isEqualTo(2)
+                isGreaterThanOrEqualTo(8)
+            }
+        }
+
+        @Test
+        fun `incomplete assertion`() {
+            expectThat(1) { }
+        }
+    }
+
+    @Nested
     inner class StringTest {
         @Test
-        fun `string value test`() {
-            val actualValue: String = STRING_VALUE_TO_TEST
-
-            expectThat(actualValue)
-                .isEqualTo(STRING_VALUE_TO_TEST)
-                .hasLength(STRING_VALUE_TO_TEST.length)
-
-            expectThat(actualValue) {
+        fun `assertions with context(AKA soft assertion)`() {
+            expectThat(STRING_VALUE_TO_TEST) {
                 isEqualTo("aaaaa")
                 isNotEqualTo(STRING_VALUE_TO_TEST)
+                matches(Regex("[\\w\\s]+"))
+                isEqualToIgnoringCase("ffffff")
+                startsWith("BBB")
+            }
+        }
+
+        @Test
+        fun `string meta`() {
+            expectThat(STRING_VALUE_TO_TEST) {
                 hasLength(35)
                 isBlank()
                 isEmpty()
-                matches(Regex("[\\w]+"))
-                startsWith("T")
+                isNotEmpty()
+                isNullOrBlank()
+                isNullOrEmpty()
+                isUpperCase()
             }
         }
     }
 
+    @Nested
+    inner class CollectionTests {
+        @Test
+        fun `collection meta test`() {
+            expectThat(LIST_1_2_3) {
+                isNotEmpty()
+                single().isEqualTo(0)
+                count().isEqualTo(3)
+                count("less then 0") { it < 0 }.isEqualTo(0)
+                isSorted()
+            }
+        }
+
+        @Test
+        fun `collection elements test`() {
+            expectThat(LIST_1_2_3) {
+                first().isEqualTo(1)
+                first { it == 1 }.isEqualTo(1)
+                withFirst {
+                    isLessThan(2)
+                }
+
+                this[0].isEqualTo(1)
+                withElementAt(0) {
+                    isLessThanOrEqualTo(2)
+                }
+
+                last().isEqualTo(3)
+            }
+        }
+
+        @Test
+        fun `elements by filter`() {
+            expectThat(LIST_1_2_3) {
+                all { isGreaterThan(0) }
+                any { isGreaterThan(0) }
+                none { isLessThan(1) }
+                atLeast(1) { isGreaterThan(0) }
+                atMost(3) { isGreaterThan(0) }
+                exactly(3) { isGreaterThan(0) }
+
+                filterIsInstance<Int>().count().isEqualTo(3)
+                filter { int -> int > 0  }.count().isEqualTo(3)
+            }
+        }
+
+        @Test
+        fun `assertions with transforming`() {
+            expectThat(EMPLOYEES) {
+                flatMap { employee -> employee.department.location.phones }.count().isEqualTo(4)
+                map { employee -> employee.name }.containsExactlyInAnyOrder("Alice", "John")
+            }
+        }
+    }
+
+    @Nested
+    inner class MapTests {
+        @Test
+        fun `meta test`() {
+            expectThat(MAP_EMPLOYEES) {
+                isEmpty()
+                isNotEmpty()
+                hasSize(2)
+            }
+        }
+
+        @Test
+        fun `keys test`() {
+            expectThat(MAP_EMPLOYEES) {
+                containsKey(1)
+                containsKeys(1, 2)
+                doesNotContainKey(3)
+            }
+        }
+
+        @Test
+        fun `value by key`() {
+            expectThat(MAP_EMPLOYEES) {
+                get(0).isNull()
+                getValue(1).isEqualTo(EMPLOYEE_JOHN)
+                withValue(2) {
+                    isEqualTo(EMPLOYEE_ALICE)
+                }
+            }
+        }
+
+        @Test
+        fun `entry test`() {
+            expectThat(MAP_EMPLOYEES).hasEntry(1, EMPLOYEE_ALICE)
+        }
+    }
 }
